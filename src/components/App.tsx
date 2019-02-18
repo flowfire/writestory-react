a702361c3bb00cb0c3268a3ca90ec310fb44b6ce
孟子易 <mengziyi540841@gmail.com>
2019-02-18 18:51:49 +0800

    Primary version. Generate a project with 'create-react-app' and change some folder structure.

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Router, Route } from "react-router";
+import logo from '../assets/logo.svg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+export default App;
